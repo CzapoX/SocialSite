b57a773722100ce5b6bfc3addfef65c119ee28b1
CzapoX <czapox@gmail.com>
2021-02-22 16:34:58 +0100

    Added getter for single post

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Application.Posts;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System;
 
 namespace API.Controllers
 {
@@ -12,6 +13,12 @@ namespace API.Controllers
         public async Task<ActionResult<List<Post>>> GetPosts()
         {
             return await Mediator.Send(new List.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetPost(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
