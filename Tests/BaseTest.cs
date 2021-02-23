498e08d9ff1e9fe35f8b6a7fd8a862c4c61a93e3
CzapoX <czapox@gmail.com>
2021-02-23 14:19:57 +0100

    Created BaseTest
    BaseTest generates InMemoryDatabase for testing

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Tests
+{
+    public class BaseTest
+    {
+        protected DataContext GetDataContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase("TestDatabase").Options;
+            var dbContext = new DataContext(options);
+            
+            return dbContext;
+        }
+    }
+}
