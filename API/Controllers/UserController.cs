3b37fa4b169abcd6d7a830ba7fc99268eff245c4
CzapoX <czapox@gmail.com>
2021-02-28 13:45:33 +0100

    Added UserController
    Added UserController with login action

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Application.User;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class UserController : BaseApiController
+    {
+        /// <summary>
+        /// Logs in existing user
+        /// </summary>
+        /// <param name="loginCredentials">User login credentials</param>
+        /// <returns></returns>
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(UserLoginDto loginCredentials)
+        {
+            return HandleResult(await Mediator.Send(new Login.Query { UserLogin = loginCredentials }));
+        }
+    }
+}
