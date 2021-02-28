b2877f2634c015e3431a68a3c5e5dd7e43be82e1
CzapoX <czapox@gmail.com>
2021-02-28 13:24:50 +0100

    Added examples for Swagger

--- Application/User/UserLoginDto.cs
+++ Application/User/UserLoginDto.cs
@@ -5,11 +5,13 @@
         /// <summary>
         /// User email
         /// </summary>
+        ///<example>norbert@test.pl</example>
         public string Email { get; set; }
 
         /// <summary>
         /// User password
         /// </summary>
+        ///<example>!Password1</example>
         public string Password { get; set; }
     }
 }
