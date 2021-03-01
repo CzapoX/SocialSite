26105555337f3a1ac6800848b8ca4a86a9348de4
CzapoX <czapox@gmail.com>
2021-03-01 12:26:49 +0100

    Fixed Login Validator

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -16,15 +16,15 @@ namespace Application.User
             public UserLoginDto loginCredentials { get; set; }
         }
 
-        public class QueryValidator : AbstractValidator<Query>
+        public class QueryValidator : AbstractValidator<UserLoginDto>
         {
             public QueryValidator()
             {
-                RuleFor(x => x.loginCredentials.Email)
+                RuleFor(x => x.Email)
                     .NotEmpty().WithMessage("Email can't be empty")
                     .EmailAddress().WithMessage("A valid email address is required.");
 
-                RuleFor(x => x.loginCredentials.Password)
+                RuleFor(x => x.Password)
                     .NotEmpty().WithMessage("Password can't be empty");
             }
         }
