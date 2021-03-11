6dba94cb4504394905825b9ce3fc31ca042f588e
CzapoX <czapox@gmail.com>
2021-03-11 13:15:04 +0100

    Added Get Profile Details by username Action

---
+++ API/Controllers/ProfilesController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ProfilesController : BaseApiController
+    {
+        /// <summary>
+        /// Fetches a single profile by username
+        /// </summary>
+        /// <param name="username" example="Norbert"></param>
+        /// <returns></returns>
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+    }
+}
---
+++ Application/Profiles/Details.cs
@@ -0,0 +1,51 @@
+﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class Details
+    {
+        public class Query : IRequest<Result<Profile>>
+        {
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<Profile>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<Profile>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var username = request.Username.ToUpperInvariant();
+                
+                var user = await _context.Users
+                    .Include(x => x.Photos)
+                    .FirstOrDefaultAsync(x => x.NormalizedUserName == username);
+
+                if (user == null)
+                    return null;
+
+                var profile = _mapper.Map<Profile>(user);
+
+                return Result<Profile>.Success(profile);
+            }
+        }
+    }
+}
