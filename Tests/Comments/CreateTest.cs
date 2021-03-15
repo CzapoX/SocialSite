3b16e4c8cc6e36bc1a821cfc070496e760eeb4d8
CzapoX <czapox@gmail.com>
2021-03-15 09:48:30 +0100

    Added Create Comment Test

--- Tests/Comments/CreateTest.cs
+++ Tests/Comments/CreateTest.cs
@@ -2,14 +2,51 @@
 using Application.Comments;
 using Moq;
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
+using Application.Core;
+using Domain;
 
 namespace Tests.Comments
 {
-    class CreateTest : BaseTest
+    public class CreateTest : BaseTest
     {
+        private readonly Mock<IUserAccessor> _userAccessor;
+        private readonly Guid postId = Guid.NewGuid();
+
+        public CreateTest()
+        {
+            _userAccessor = new Mock<IUserAccessor>();
+            _userAccessor.Setup(u => u.GetCurrentUserId()).Returns("123");
+        }
+
+        [Fact]
+        public async Task ShouldCreateComment()
+        {
+            var context = GetDataContext();
+            var sut = new Create.Handler(context, _userAccessor.Object, _mapper);
+
+            context.Posts.Add(new Post { Id = postId, Title = "Test 1" });
+            context.Users.Add(new AppUser
+            {
+                Id = "123",
+                Email = "test@test.com",
+                UserName = "test"
+            });
+            context.SaveChanges();
+
+            var commentCommand = new Create.Command
+            {
+                PostId = postId,
+                Content = "Example"
+            };
+
+            var result = await sut.Handle(commentCommand, CancellationToken.None);
+
+            Assert.NotNull(result.Value);
+            Assert.Equal(ResultStatus.IsSuccess, result.ResultStatus);
+            Assert.Equal("Example", result.Value.Content);
+        }
     }
 }
