8216e4227ca9caf82aaa904dbce4a95e6337ae34
CzapoX <czapox@gmail.com>
2021-03-15 10:31:30 +0100

    Update ListTest.cs
    Created List Comments Test

--- Tests/Comments/ListTest.cs
+++ Tests/Comments/ListTest.cs
@@ -1,15 +1,44 @@
-﻿using Application.Interfaces;
-using Application.Comments;
-using Moq;
-using System;
-using System.Linq;
+﻿using Application.Comments;
+using Domain;
 using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
+using System;
+using System.Collections.Generic;
 
 namespace Tests.Comments
 {
-    class ListTest : BaseTest
+    public class ListTest : BaseTest
     {
+        private readonly Guid postId = Guid.NewGuid();
+
+        [Fact]
+        public void ShouldReturnListOfComments()
+        {
+            var context = GetDataContext();
+
+            context.Users.Add(new AppUser
+            {
+                Id = "1",
+                Email = "test@test.com",
+                UserName = "test",
+                Bio ="testBio",
+                Photos = new List<Photo>()
+            });
+
+            context.Posts.Add(new Post { Id = postId, Title = "Post with Comments", PostOwnerId = "1" });
+            context.Comments.Add(new Comment { Content = "test1", AuthorId = "1", PostId = postId, CreateDate = DateTime.UtcNow });
+            context.Comments.Add(new Comment { Content = "test2", AuthorId = "1", PostId = postId, CreateDate = DateTime.UtcNow });
+
+            context.SaveChanges();
+
+            var sut = new List.Handler(context, _mapper);
+            var result = sut.Handle(new List.Query { PostId = postId}, CancellationToken.None).Result.Value;
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.Equal("test1", result[0].Content);
+            Assert.Equal("test2", result[1].Content);
+            Assert.Equal("test", result[0].Author.Username);
+        }
     }
 }
